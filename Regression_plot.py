b435532ff46b35698297f93c0dbb95927b808a1d
JoanZ0019 <qzz0019@auburn.edu>
2022-03-20 21:09:34 -0500

    reusable script

--- Regression_plot.py
+++ Regression_plot.py
@@ -1,17 +1,39 @@
+#!/usr/bin/env python
+
+import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
+
+def species(n):
+    x = n.petal_length_cm
+    y = n.sepal_length_cm
+    regression = stats.linregress(x, y)
+    slope = regression.slope
+    intercept = regression.intercept
+    plt.scatter(x, y, label = 'Data')
+    plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.legend()
+    plt.savefig("petal_v_sepal_length_regress.png")
+
+"""
+Generate the regression plot
+
+Parameter
+------
+n: species
+------
+"""
+
 dataframe = pd.read_csv("iris.csv")
-versicolor = dataframe[dataframe.species == 'Iris_versicolor']
-x = versicolor.petal_length_cm
-y = versicolor.sepal_length_cm
-regression = stats.linregress(x, y)
-slope = regression.slope
-intercept = regression.intercept
-plt.scatter(x, y, label = 'Versicolor')
-plt.plot(x, slope * x + intercept, color = "orange", label = 'Fitted line')
-plt.xlabel("Petal length (cm)")
-plt.ylabel("Sepal length (cm)")
-plt.legend()
-plt.savefig("petal_v_sepal_length_regress_versicolor.png")
-quit()
+#versicolor = dataframe[dataframe.species == 'Iris_versicolor']
+#species(versicolor)
+
+#setosa = dataframe[dataframe.species == 'Iris_setosa']
+#species(setosa)
+ 
+virginica = dataframe[dataframe.species == 'Iris_virginica']
+species(virginica)
+
